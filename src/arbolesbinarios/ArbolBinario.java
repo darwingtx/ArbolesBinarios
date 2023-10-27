19a1956b23c12a202d92b73f3f99391f0347b021
Alerca <alejoariascorrea@gmail.com>
2023-10-27 07:59:52 -0500

    Idente el codigo

--- src/arbolesbinarios/ArbolBinario.java
+++ src/arbolesbinarios/ArbolBinario.java
@@ -3,76 +3,112 @@ package arbolesbinarios;
 import javax.swing.JOptionPane;
 
 public class ArbolBinario {
-    
+
     private Nodo Raiz;
 
-    public ArbolBinario(char vc[]){
+    public ArbolBinario(char vc[]) {
+
         char mayor = vc[0];
         Raiz = new Nodo(mayor);
 
         for (char c : vc) {
+
             if (mayor < c) {
+
                 InsertarMayor(c);
-            }else if(mayor>c){
+
+            } else if (mayor > c) {
+
                 InsertarMenor(c);
             }
         }
 
     }
 
-    public void InsertarMayor(char c){
+    public void InsertarMayor(char c) {
+
         Nodo p = Raiz.getLigaDer();
         Nodo x = new Nodo(c);
+
         if (p == null) {
+
             Raiz.setLigaDer(x);
-        }else{
+
+        } else {
+
             Nodo a = p;
-            while (p!=null) {
+
+            while (p != null) {
+
                 a = p;
+
                 if (p.getDato() > c) {
+
                     p = p.getLigaIzq();
-                }else{
+
+                } else {
+
                     p = p.getLigaDer();
                 }
             }
-            
-            if (a.getDato()>c) {
+
+            if (a.getDato() > c) {
+
                 a.setLigaIzq(x);
-            }else{
+
+            } else {
+
                 a.setLigaDer(x);
             }
 
         }
     }
 
-    public void InsertarMenor(char c){
+    public void InsertarMenor(char c) {
+
         Nodo p = Raiz.getLigaIzq();
         Nodo x = new Nodo(c);
+
         if (p == null) {
+
             Raiz.setLigaIzq(x);
-        }else{
+
+        } else {
+
             Nodo a = p;
-            while (p!=null) {
+
+            while (p != null) {
+
                 a = p;
+
                 if (p.getDato() < c) {
+
                     p = p.getLigaIzq();
-                }else{
+
+                } else {
+
                     p = p.getLigaDer();
                 }
             }
-            
-            if (a.getDato()>c) {
+
+            if (a.getDato() > c) {
+
                 a.setLigaIzq(x);
-            }else{
+
+            } else {
+
                 a.setLigaDer(x);
             }
 
         }
     }
-    public void Recorrer(Nodo R){
-        if(R!=null){
+
+    public void Recorrer(Nodo R) {
+
+        if (R != null) {
+
             Recorrer(R.getLigaIzq());
-           JOptionPane.showMessageDialog(null, R.getDato());
+            JOptionPane.showMessageDialog(null, R.getDato());
             Recorrer(R.getLigaDer());
         }
 
