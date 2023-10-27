36368d91063c76dc3a8b0c81ac76fab452b570e0
Darwin Castaño <dc162005@gmail.com>
2023-10-27 08:28:45 -0500

    Listo el crear arbol

--- src/arbolesbinarios/ArbolBinario.java
+++ src/arbolesbinarios/ArbolBinario.java
@@ -12,52 +12,32 @@ public class ArbolBinario {
         Raiz = new Nodo(mayor);
 
         for (char c : vc) {
-
             if (mayor < c) {
-
                 InsertarMayor(c);
-
             } else if (mayor > c) {
-
                 InsertarMenor(c);
             }
         }
-
     }
 
     public void InsertarMayor(char c) {
-
         Nodo p = Raiz.getLigaDer();
         Nodo x = new Nodo(c);
-
         if (p == null) {
-
             Raiz.setLigaDer(x);
-
         } else {
-
             Nodo a = p;
-
             while (p != null) {
-
                 a = p;
-
                 if (p.getDato() > c) {
-
                     p = p.getLigaIzq();
-
                 } else {
-
                     p = p.getLigaDer();
                 }
             }
-
             if (a.getDato() > c) {
-
                 a.setLigaIzq(x);
-
             } else {
-
                 a.setLigaDer(x);
             }
 
@@ -65,37 +45,23 @@ public class ArbolBinario {
     }
 
     public void InsertarMenor(char c) {
-
         Nodo p = Raiz.getLigaIzq();
         Nodo x = new Nodo(c);
-
         if (p == null) {
-
             Raiz.setLigaIzq(x);
-
         } else {
-
             Nodo a = p;
-
             while (p != null) {
-
                 a = p;
-
                 if (p.getDato() < c) {
                     p = p.getLigaDer();
                 }else{
                     p = p.getLigaIzq();
-                }else{
-                    p = p.getLigaDer();
                 }
             }
-
             if (a.getDato() > c) {
-
                 a.setLigaIzq(x);
-
             } else {
-
                 a.setLigaDer(x);
             }
 
@@ -109,40 +75,35 @@ public class ArbolBinario {
             Recorrer(R.getLigaIzq());
             JOptionPane.showMessageDialog(null, R.getDato());
             Recorrer(R.getLigaDer());
-  
-     public void RecorrerPre(Nodo R){
-        if(R!=null){
+
+        }
+    }
+    public void RecorrerPre(Nodo R) {
+        if (R != null) {
             JOptionPane.showMessageDialog(null, R.getDato());
             RecorrerPre(R.getLigaIzq());
             RecorrerPre(R.getLigaDer());
         }
 
     }
-      public void RecorrerIN(Nodo R){
-        if(R!=null){
+
+    public void RecorrerIN(Nodo R) {
+        if (R != null) {
             RecorrerIN(R.getLigaIzq());
-           JOptionPane.showMessageDialog(null, R.getDato());
+            JOptionPane.showMessageDialog(null, R.getDato());
             RecorrerIN(R.getLigaDer());
         }
 
     }
-      public void RecorrerIN(Nodo R){
-        if(R!=null){
-            RecorrerIN(R.getLigaIzq());
-           JOptionPane.showMessageDialog(null, R.getDato());
-            RecorrerIN(R.getLigaDer());
-        }
 
-    }
-     public void RecorrerPos(Nodo R){
-        if(R!=null){
+    public void RecorrerPos(Nodo R) {
+        if (R != null) {
             RecorrerPos(R.getLigaIzq());
             RecorrerPos(R.getLigaDer());
-             JOptionPane.showMessageDialog(null, R.getDato());
+            JOptionPane.showMessageDialog(null, R.getDato());
         }
 
     }
-
 
     public Nodo getRaiz() {
         return Raiz;
