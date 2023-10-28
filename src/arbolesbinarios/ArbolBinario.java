88c6c2ddfcdd17dee086ab643ec26bd4e72afc8f
Andres Alvarez <andravid019@gmail.com>
2023-10-28 15:00:41 -0500

    profundidad, altura y padre

--- src/arbolesbinarios/ArbolBinario.java
+++ src/arbolesbinarios/ArbolBinario.java
@@ -3,128 +3,186 @@ package arbolesbinarios;
 import javax.swing.JOptionPane;
 
 public class ArbolBinario {
-    
+
     private Nodo Raiz;
 
-    public ArbolBinario(char vc[]){
+    public ArbolBinario(char vc[]) {
         char mayor = vc[0];
         Raiz = new Nodo(mayor);
 
         for (char c : vc) {
             if (mayor < c) {
                 InsertarMayor(c);
-            }else if(mayor>c){
+            } else if (mayor > c) {
                 InsertarMenor(c);
             }
         }
 
     }
 
-    public void InsertarMayor(char c){
+    public void InsertarMayor(char c) {
         Nodo p = Raiz.getLigaDer();
         Nodo x = new Nodo(c);
         if (p == null) {
             Raiz.setLigaDer(x);
-        }else{
+        } else {
             Nodo a = p;
-            while (p!=null) {
+            while (p != null) {
                 a = p;
                 if (p.getDato() > c) {
                     p = p.getLigaIzq();
-                }else{
+                } else {
                     p = p.getLigaDer();
                 }
             }
-            
-            if (a.getDato()>c) {
+
+            if (a.getDato() > c) {
                 a.setLigaIzq(x);
-            }else{
+            } else {
                 a.setLigaDer(x);
             }
 
         }
     }
 
-    public void InsertarMenor(char c){
+    public void InsertarMenor(char c) {
         Nodo p = Raiz.getLigaIzq();
         Nodo x = new Nodo(c);
         if (p == null) {
             Raiz.setLigaIzq(x);
-        }else{
+        } else {
             Nodo a = p;
-            while (p!=null) {
+            while (p != null) {
                 a = p;
                 if (p.getDato() < c) {
                     p = p.getLigaIzq();
-                }else{
+                } else {
                     p = p.getLigaDer();
                 }
             }
-            
-            if (a.getDato()>c) {
+
+            if (a.getDato() > c) {
                 a.setLigaIzq(x);
-            }else{
+            } else {
                 a.setLigaDer(x);
             }
 
         }
     }
-  
-     public String  RecorrerPre(Nodo R){
-         String s="";
-        if(R!=null){
+
+    public String RecorrerPre(Nodo R) {
+        String s = "";
+        if (R != null) {
             // JOptionPane.showMessageDialog(null, R.getDato());
-            s+=R.getDato();
-            s+=RecorrerPre(R.getLigaIzq());
-            s+=RecorrerPre(R.getLigaDer());
+            s += R.getDato();
+            s += RecorrerPre(R.getLigaIzq());
+            s += RecorrerPre(R.getLigaDer());
         }
-       
-        return s;
-            
-    }
-      public String RecorrerIN(Nodo R){
-        String s="";
-        if(R!=null){
-            s+=RecorrerIN(R.getLigaIzq());
-            s+=R.getDato();
-            s+=RecorrerIN(R.getLigaDer());
-        }
-    return s;
-    }
-     public String RecorrerPos(Nodo R){
-         String s="";
-        if(R!=null){
-            s+=RecorrerPos(R.getLigaIzq());
-            s+=RecorrerPos(R.getLigaDer());
-            s+=R.getDato();
-        }
-    return s;
-    }
-    public String Mostrarhojas(Nodo R){
-         String s="";
-        if(R!=null){
 
-         if(R.getLigaIzq()== null && R.getLigaDer()==null){
-             s+=R.getDato();
-            }
-           s+= Mostrarhojas(R.getLigaIzq());
-            s+=Mostrarhojas(R.getLigaDer());
+        return s;
+
+    }
+
+    public String RecorrerIN(Nodo R) {
+        String s = "";
+        if (R != null) {
+            s += RecorrerIN(R.getLigaIzq());
+            s += R.getDato();
+            s += RecorrerIN(R.getLigaDer());
         }
         return s;
     }
-        
-    public String Mostrarpadres(Nodo R){
-         String s="";
-        if(R!=null){
 
-         if(R.getLigaIzq()!= null || R.getLigaDer()!=null){
-             s+=R.getDato();
-            }
-          s+=  Mostrarpadres(R.getLigaIzq());
-           s+=Mostrarpadres(R.getLigaDer());
+    public String RecorrerPos(Nodo R) {
+        String s = "";
+        if (R != null) {
+            s += RecorrerPos(R.getLigaIzq());
+            s += RecorrerPos(R.getLigaDer());
+            s += R.getDato();
         }
-            return s;
+        return s;
     }
+
+    public String Mostrarhojas(Nodo R) {
+        String s = "";
+        if (R != null) {
+
+            if (R.getLigaIzq() == null && R.getLigaDer() == null) {
+                s += R.getDato();
+            }
+            s += Mostrarhojas(R.getLigaIzq());
+            s += Mostrarhojas(R.getLigaDer());
+        }
+        return s;
+    }
+
+    public String Mostrarpadres(Nodo R) {
+        String s = "";
+        if (R != null) {
+
+            if (R.getLigaIzq() != null || R.getLigaDer() != null) {
+                s += R.getDato();
+            }
+            s += Mostrarpadres(R.getLigaIzq());
+            s += Mostrarpadres(R.getLigaDer());
+        }
+        return s;
+    }
+
+    public int Altura(Nodo R) {
+        int Altura = 0;
+        if (R != null) {
+            Altura = Math.max(Altura, Altura(R.getLigaIzq()));
+            Altura = Math.max(Altura, Altura(R.getLigaDer()));
+
+            Altura++;
+        }
+
+        return Altura;
+    }
+
+    public int Profundo(Nodo R){
+        Nodo p =null;
+        int deep=0;
+        if(R!=this.getRaiz()){
+            deep = 1+ Profundo(this.Padre(this.Raiz, R, p));
+        }
+
+        return deep;
+    }
+
+    public void ancestros(Nodo R){
+        Nodo q=null;
+         int i=0; String s="";
+         i=this.Profundo(R);
+         while(i>0){
+          s+= this.Padre(this.Raiz, R, q).getDato() ;
+          R = this.Padre(this.Raiz, R, q);
+          i--;
+         }
+         JOptionPane.showMessageDialog(null, s);
+
+    }
+
+    public Nodo Padre(Nodo R, Nodo P,Nodo s){
+       if(R!=null && P!=null){
+
+       if(R.getLigaDer()== P || R.getLigaIzq()==P){
+        s = R;
+         }     
+        s = Padre(R.getLigaIzq(),P,s);
+        s = Padre(R.getLigaDer(),P,s);
+        }   
+
+        return s;
+    }
+   public void MostrarArbol(){
+     
+    
+
+
+
+   }
 
 
     public Nodo getRaiz() {
