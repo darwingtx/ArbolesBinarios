359d23c3a2358a585646dba1a75ae17b7de14795
Andres Alvarez <andravid019@gmail.com>
2023-10-28 15:00:41 -0500

    profundidad, altura y padre

--- src/arbolesbinarios/ArbolesBinarios.java
+++ src/arbolesbinarios/ArbolesBinarios.java
@@ -6,25 +6,32 @@ import javax.swing.JOptionPane;
 public class ArbolesBinarios {
 
    
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
         String x = JOptionPane.showInputDialog("Ingrese la cadena");
         char vc[] = x.toCharArray();
         String s="";
         ArbolBinario p = new ArbolBinario(vc);
+        Nodo r=p.getRaiz().getLigaDer().getLigaDer().getLigaDer();
      
-       s= p.RecorrerIN(p.getRaiz());
-       JOptionPane.showMessageDialog(null, s);
+    //    s= p.RecorrerIN(p.getRaiz());
+    //    JOptionPane.showMessageDialog(null, s);
         
-          s= p.RecorrerPre(p.getRaiz());
-       JOptionPane.showMessageDialog(null, s);
-       s= p.RecorrerPos(p.getRaiz());
-         JOptionPane.showMessageDialog(null, s);
-         s=p.Mostrarpadres(p.getRaiz());
-         JOptionPane.showMessageDialog(null, s);
-                 s=p.Mostrarhojas(p.getRaiz());
-         JOptionPane.showMessageDialog(null, s);
-        
+    //       s= p.RecorrerPre(p.getRaiz());
+    //    JOptionPane.showMessageDialog(null, s);
+    //    s= p.RecorrerPos(p.getRaiz());
+    //      JOptionPane.showMessageDialog(null, s);
+    //      s=p.Mostrarpadres(p.getRaiz());
+    //      JOptionPane.showMessageDialog(null, s);
+    //              s=p.Mostrarhojas(p.getRaiz());
+    //      JOptionPane.showMessageDialog(null, s);
+  
+    JOptionPane.showMessageDialog(null,p.Profundo(r));
+
     }
-    
 }
+    
+
